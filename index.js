269de2f815297e2a02718d3fccacc3424c66980a
AllisonAnz <AllisonAnzalone@gmail.com>
2021-04-10 16:13:35 -0500

    commit tree 8305ad65c6b42559c8a1fcd1d4f92fd7729c597b

---
+++ index.js
@@ -0,0 +1,14 @@
+//has a function expression called divide 
+//divide divides 2000 by 100
+
+const divide = () => 2000/100;
+
+//has an arrow function called square 
+//square arrow function takes one parameter and 
+//multiplies it times itself 
+const square = (parameter1) => parameter1 ** parameter1;
+
+//has an arrow function called add 
+//add arrow function takes two parameters 
+//and adds them together 
+const add = (parameter1, parameter2) => parameter1 + parameter2;
